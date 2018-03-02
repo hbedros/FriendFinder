7090a36954fabb97e68af7381ad3791279d75edb
haigbedros <haigbedros@gmail.com>
2018-03-02 17:00:03 -0500

    make it better

--- server.js
+++ server.js
@@ -9,7 +9,20 @@ var htmlRoutes = require('./app/routing/html-routes.js');
 
 // Set up Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+
+// Server Routing Map 
+apiRoutes(app);
+htmlRoutes(app);
+
 
 // Listener - Start the server
 app.listen(PORT, function() {
