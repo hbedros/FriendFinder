c54a47b94a489018e4f78b872c8caf764110741e
haigbedros <haigbedros@gmail.com>
2018-03-02 14:56:08 -0500

    set up routes A GET Route to /survey which should display the survey page. A default USE route that leads to home.html which displays the home page. Export for use in main server.js file

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ var htmlRoutes = require('./app/routing/html-routes.js');
 // Set up Express App
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+// Listener - Start the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
